1024a412d516d19bf7b7f2f417c860a43151ea30
Huyvictory <vominhhuy008@gmail.com>
2024-10-13 22:09:55 +0700

    feat: Update caching for application

---
+++ src/main/java/com/online/course/management/project/config/CacheConfig.java
@@ -0,0 +1,30 @@
+package com.online.course.management.project.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(60, TimeUnit.MINUTES)
+                .initialCapacity(100)
+                .maximumSize(500);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
